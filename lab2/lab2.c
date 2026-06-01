b1311cc579d9130af146f566c9a7355bd17e34cb
Ella Long <47978127+NekoAkari@users.noreply.github.com>
2026-05-31 18:28:55 -0700

    Lab02 works done

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -2,6 +2,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(void) {
   char *line = NULL;
@@ -17,7 +21,37 @@ int main(void) {
       break;
     }
 
-    printf("You typed: %s", line);
+    if (line[read - 1] == '\n') {
+      line[read - 1] = '\0';
+    }
+
+    if (line[0] == '\0') {
+      break;
+    }
+
+    if (strcmp(line, "exit") == 0) {
+      break;
+    }
+
+    if (strcmp(line, "joke") == 0) {
+      printf("Why do programmers prefer dark mode?\nBecause light attracts bugs.");
+      continue;
+    }
+
+    pid_t pid = fork();
+
+    if (pid == -1) {
+      perror("fork");
+      continue;
+    }
+
+    if (pid == 0) {
+      execl(line, line, (char *)NULL);
+      printf("Exec failure\n");
+      exit(1);
+    }
+
+    waitpid(pid, NULL, 0);
   }
 
   free(line);
